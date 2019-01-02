c93b0402ba8884cecd4345a41c7f1c55edb86ced
Hav Kokfong <havkokfong@my.jcu.edu.au>
2019-01-02 10:17:57 +0800

    Added title

--- main.py
+++ main.py
@@ -1,16 +1,25 @@
 """
-Name:
+Name: Hav Kokfong
 Date:
 Brief Project Description:
 GitHub URL:
 """
 
 from kivy.app import App
+from kivy.lang import Builder
 
 # Create your main program in this file, using the SongsToLearnApp class
 
 
 class SongsToLearnApp(App):
     pass
+
+    def build(self):
+        self.title = "Song To learn 2.0"
+        self.root = Builder.load_file('app.kv')
+        return self.root
+
+
+
 
 SongsToLearnApp().run()
