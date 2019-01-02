69c1e252e694ed6020d1aaf3894d3ec22a2f303a
Kokfong Hav <havkokfog@my.jcu.edu.au>
2019-01-02 21:38:04 +0800

    add def sorting

--- main.py
+++ main.py
@@ -7,19 +7,20 @@ GitHub URL:
 
 from kivy.app import App
 from kivy.lang import Builder
+from song import Song
+from songlist import SongList
 
 # Create your main program in this file, using the SongsToLearnApp class
 
 
 class SongsToLearnApp(App):
-    pass
 
     def build(self):
         self.title = "Song To learn 2.0"
         self.root = Builder.load_file('app.kv')
         return self.root
 
-
+    def sorting(self):
 
 
 SongsToLearnApp().run()
