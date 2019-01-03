a4c0da9958dc580c56d4bd0fd1e5fb937d60a376
Hav Kokfong <havkokfong@my.jcu.edu.au>
2019-01-03 15:02:02 +0800

    add show song code

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ GitHub URL:
 
 from kivy.app import App
 from kivy.lang import Builder
+from kivy.uix.button import Button
 from song import Song
 from songlist import SongList
 
@@ -14,13 +15,45 @@ from songlist import SongList
 
 
 class SongsToLearnApp(App):
+    song_list = SongList()
+    all_widget = []
 
     def build(self):
         self.title = "Song To learn 2.0"
         self.root = Builder.load_file('app.kv')
+        song_list = self.load_songs()
+        self.show_song(song_list)
         return self.root
 
-    def sorting(self):
+    def load_songs(self):
+        result_list = self.song_list.load_song('songs.csv')
+        return result_list
+
+    def show_song(self, display_order):
+        for i in display_order:
+            song_id = i.title
+            display = '"{0}" by {1} ({2}) {3}'.format(i.title, i.artist, i.year, i.require)
+            song_button = Button(id=song_id, text=display, color='')
+            # self.all_widget.append(song_button)
+            song_button.bind(on_release=self.select)
+            self.root.ids.all_song.add_widget(song_button)
+
+    def select(self, instance):
+        song_title = instance.id
+        self.root.ids.program_detail.color = (1,1,0,1)
+        self.root.ids.program_detail.text = "{} is selected".format(song_title)
+
+    def sorting(self, available_choice):
+        if available_choice == 'Title':
+            sort_song = self.song_list.sort(0)
+        elif available_choice == 'Artist':
+            sort_song = self.song_list.sort(1)
+        elif available_choice == 'Year':
+            sort_song = self.song_list.sort(2)
+        else:
+            sort_song = self.song_list.sort(3)
+        self.show_song(sort_song)
+
 
 
 SongsToLearnApp().run()
