5886c686406a4720ef1ead6ff6fa84918f8d6969
Kokfong Hav <havkokfog@my.jcu.edu.au>
2019-01-05 23:49:40 +0800

    Complete test song

--- test_song.py
+++ test_song.py
@@ -6,12 +6,20 @@ song = Song()
 print(song)
 assert song.artist == ""
 assert song.title == ""
-assert song.year == 0
-assert song.is_required
+assert song.year == ""
+assert song.require == ""
 
 # test initial-value song
-song2 = Song("Amazing Grace", "John Newton", 1779, True)
+song2 = Song("Amazing Grace", "John Newton", 1779, 'y')
+assert song2.title == "Amazing Grace"
+assert song2.artist == "John Newton"
+assert song2.year == 1779
+assert song2.require == 'y'
+
 # TODO: write tests to show this initialisation works
+print("{} by {} in {}".format(song2.title, song2.artist, song2.year))
 
 # test mark_learned()
 # TODO: write tests to show the mark_learned() method works
+song2.marked_learn()
+print(song2)
