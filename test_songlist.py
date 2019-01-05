1b088f8b53cf8bb446ccc8e98d7032275c2888df
Hav Kokfong <havkokfong@my.jcu.edu.au>
2019-01-05 14:48:59 +0800

    Checking sorting function

--- test_songlist.py
+++ test_songlist.py
@@ -7,16 +7,20 @@ from song import Song
 # test empty SongList
 song_list = SongList()
 print(song_list)
-assert len(song_list.songs) == 0
+assert len(song_list.song) == 0
 
 # test loading songs
-song_list.load_songs('songs.csv')
+song_list.load_song('songs.csv')
 print(song_list)
-assert len(song_list.songs) > 0  # assuming CSV file is not empty
+assert len(song_list.song) > 0  # assuming CSV file is not empty
 
 # TODO: add tests below to show the various required methods work as expected
 # test sorting songs
+print("Sorting by year")
+song_list.sort('year')
 
+print("Sorting by title")
+song_list.sort('title')
 # test adding a new Song
 
 # test get_song()
@@ -24,3 +28,4 @@ assert len(song_list.songs) > 0  # assuming CSV file is not empty
 # test getting the number of required and learned songs (separately)
 
 # test saving songs (check CSV file manually to see results)
+
