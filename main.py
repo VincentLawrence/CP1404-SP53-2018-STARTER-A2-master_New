c2acfac7f0a4301e681bc445156a8f7d02ab8cba
Hav Kokfong <havkokfong@my.jcu.edu.au>
2019-01-07 20:14:12 +0800

    Check dupicate song

--- main.py
+++ main.py
@@ -78,6 +78,7 @@ class SongsToLearnApp(App):
         title = self.root.ids.title_fill.text
         artist = self.root.ids.artist_fill.text
         year = self.year_check()
+        validate = True
         if title == '' or artist == '' or year == '':  # No input validation
             self.root.ids.program_detail.color = ERROR_COLOUR
             self.root.ids.program_detail.text = 'Please fill every box'
@@ -87,6 +88,10 @@ class SongsToLearnApp(App):
         elif year < 0:                  # Year validation
             self.root.ids.program_detail.color = ERROR_COLOUR
             self.root.ids.program_detail.text = 'Year must have at least 4 digits'
+        elif validate is True:
+            self.clear_all()
+            self.root.ids.program_detail.color = ERROR_COLOUR
+            self.root.ids.program_detail.text = 'Song already in the list'
         else:
             song_title = self.root.ids.title_fill.text
             song_artist = self.root.ids.artist_fill.text
@@ -106,6 +111,12 @@ class SongsToLearnApp(App):
         except ValueError:
             year = 'string'
             return year
+
+    def song_check(self):
+        for song in self.song_list.song:
+            if song.title is self.root.ids.title_fill.text and song.artist is self.root.ids.artist_fill.text \
+                    and song.year is self.root.ids.year_fill.text:
+                return True
 
     def clear_all(self):    # Clear input in text input function
         self.root.ids.title_fill.text = ''
